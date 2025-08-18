d6d8b9b683d41770ed023d2da0f1315c3d80cd58
sahil25123 <sahilgupta25123@gmail.com>
2025-08-18 20:25:18 +0530

    Fix MongoDB connection by using the correct URI from environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connect =()=>{
     const MONGO_URI=process.env.MONGO_URI
-    console.log(MONGO_URI)
+    // console.log(MONGO_URI)
     try{
-        mongoose.connect("")
+        mongoose.connect(MONGO_URI)
+        console.log("Connected to MongoDB Successfully")
 
 
     }
